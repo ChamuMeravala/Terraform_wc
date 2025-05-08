b7ac22de3512d42eb79341c2c9591e41b5865e01
ChamuMeravala <169004302+ChamuMeravala@users.noreply.github.com>
2025-05-08 05:42:41 +0530

    Update config.tf
    added name of s3 bucket

--- config.tf
+++ config.tf
@@ -1,3 +1,7 @@
 provider "aws" {
     region = "${var.AWS_REGION}"
 }
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "chamu-s3-bucket"  # Updated bucket name
+  acl    = "private"  # Setting the ACL to private for security
+}
